23150f123ded68b8ecc09d5f7338c6f32e28c700
Rafael-Pierre <pierredoc28@gmail.com>
2023-02-15 10:46:31 -0300

    adicionando o header da pagina

--- application.py
+++ application.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def tela_inicial():
     return render_template("index.html")
 
-@app.route('/login')
-def tela_login():
+@app.route('/login', methods=["GET", "POST"])
+def login():
     return render_template("login.html")
 
-@app.route('/exibe_stp')
-def tela_exibe():
-    return render_template("exibe.html")
+@app.route('/home', methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
 
+@app.route('/cadastra', methods=["GET", "POST"])
+def cadastra():
+    return render_template("cadastra.html")
+
+@app.route('/sobre', methods=["GET", "POST"])
+def sobre():
+    return render_template("sobre.html")
 
