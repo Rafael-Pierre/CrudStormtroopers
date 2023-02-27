6bb56f9e1c18d1f2ec037111f34f139a488e5817
Rafael-Pierre <pierredoc28@gmail.com>
2023-02-26 22:15:40 -0300

    adicionando botao de cadastro + ajustes no banco

--- db/conecta_db.py
+++ db/conecta_db.py
@@ -1,17 +1,19 @@
 import psycopg2
 
-host      = 'localhost'
+host      = 'containers-us-west-147.railway.app'
 user      = 'postgres'
-password  = 'crud123'
-database  = 'postgres'
+password  = 'yKzfd7b34pPugCB0fssf'
+database  = 'railway'
+port      = 5608
 
-string_conexao = "host={0} user={1} dbname={2} password={3}".format(host, user, database, password)
+string_conexao = "user={0} password={1} host={2} port={3} dbname={4}".format( user, password, host, port, database)
+#string_conexao = 'postgresql://${{postgres}}:${{yKzfd7b34pPugCB0fssf}}@${{containers-us-west-147.railway.app}}:${{5608}}/${{railway}}'
 conn = psycopg2.connect(string_conexao)
 print("Connection established")
 
 cursor = conn.cursor()
 
-comando = 'select * from stormtrooper where numero = 1;'
+comando = 'select * from stormtrooper'
 
 cursor.execute(comando)
 
